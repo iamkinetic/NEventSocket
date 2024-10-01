14923cbe212ec36880ec76a3051644ebce80911c
Eric Fortin <eric.fortin@cauca.ca>
2024-10-01 09:51:44 -0400

    SOFT-1881 Timeout on SendApi

--- NEventSocket/Sockets/EventSocket.cs
+++ NEventSocket/Sockets/EventSocket.cs
@@ -151,6 +151,11 @@ namespace NEventSocket.Sockets
                 subscriptions.Add(
                     Messages.Where(x => x.ContentType == ContentTypes.ApiResponse)
                             .Take(1)
+                            .Timeout(
+                                ResponseTimeOut,
+                                Observable.Throw<BasicMessage>(
+                                    new TimeoutException(
+                                        "No api response within the specified timeout of {0}.".Fmt(ResponseTimeOut))))
                             .Select(x => new ApiResponse(x))
                             .Do(
                                 m =>
